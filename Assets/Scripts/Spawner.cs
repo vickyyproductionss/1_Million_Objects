ae2e310da96c714a5bddac4efc478c0f4e1c153d
vicky chaudhary <vickychaudhary8955@gmail.com>
2024-07-07 02:41:38 +0530

    Simple On The Ground

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -12,13 +12,13 @@ public class Spawner : MonoBehaviour
 
     IEnumerator SpawnAnotherBalls()
     {
-        yield return new WaitForSeconds(.1f);
+        yield return new WaitForSeconds(.3f);
         GameObject ball1 = Instantiate(BallPrefab,transform);
-        //ball1.GetComponent<Rigidbody>().AddForce(Vector3.up*(Random.Range(10, 30)));
+        ball1.GetComponent<Rigidbody>().AddForce(Vector3.one*(Random.Range(10, 30)));
         GameManager.instance.AddCount();
-        yield return new WaitForSeconds(.1f);
+        yield return new WaitForSeconds(.3f);
         GameObject ball2 = Instantiate(BallPrefab,transform);
-        //ball2.GetComponent<Rigidbody>().AddForce(Vector3.up*(Random.Range(10, 30)));
+        ball2.GetComponent<Rigidbody>().AddForce(Vector3.one*(Random.Range(10, 30)));
         GameManager.instance.AddCount();
     }
 }
